2ea3a35d7de64e447b8960b53a33c14afd7d2056
nimmel2s <nico.immel@smail.inf.h-brs.de>
2023-05-05 03:14:51 +0200

    Refactoring main2: added some comments, deleted some lines

--- Main2.java
+++ Main2.java
@@ -5,17 +5,15 @@ import java.util.Arrays;
 
 public class Main2 {
     public static void main(String[] args) {
+        //just change filepath, neural net creation and weight setting is automated
         double[][] data = CSVReader.read("Tests/csv/Test.csv");
         NeuronalNetwork nn = new NeuronalNetwork();
         int[] structure = new int[data[0].length];
-        int csv_weights_length = 0;
 
         for(int i = 0; i < structure.length; i++) {
             structure[i] = (int)data[0][i];
-            if(i < structure.length - 1) {
-                csv_weights_length += (int)data[0][i];
-            }
         }
+
         nn.create(structure);
 
         double[][][]weights = CSVReader.formatWeightMatrix(data);
