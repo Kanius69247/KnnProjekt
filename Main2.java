57536dd9e2629273f8631745a459d1c92db5ed65
nimmel2s <nico.immel@smail.inf.h-brs.de>
2023-05-12 12:57:21 +0200

    Updated main2, made it more readable and user-friendly

--- Main2.java
+++ Main2.java
@@ -2,49 +2,63 @@ import NeuronalNetwork.CSVReader;
 import NeuronalNetwork.NeuronalNetwork;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main2 {
     public static void main(String[] args) {
-        //just change filepath, neural net creation and weight setting is automated
-        double[][] data = CSVReader.read("Tests/csv/TrafficLightStructure.csv");
-        double[][] results = CSVReader.read("Tests/csv/TrafficLightResults.csv");
-        NeuronalNetwork nn = new NeuronalNetwork();
-        int[] structure = new int[data[0].length];
+        Scanner reader = new Scanner(System.in);
 
+        //read in the data
+        System.out.print("Structure CSV Filename (.csv): ");
+        String line = reader.next();
+        double[][] data = CSVReader.read("Tests/csv/" + line);
+
+        System.out.print("Structure CSV Filename (.csv): ");
+        line = reader.next();
+        double[][] results = CSVReader.read("Tests/csv/" + line);
+
+        //create new network instance
+        NeuronalNetwork nn = new NeuronalNetwork();
+
+        //get the structure
+        int[] structure = new int[data[0].length];
         for(int i = 0; i < structure.length; i++) {
             structure[i] = (int)data[0][i];
         }
 
+        //create network with specified structure
         nn.create(structure);
 
+        //set the weights
         nn.setWeights(CSVReader.formatWeightMatrix(data));
 
-        System.out.println(nn + "\n");
+        System.out.println("\nNeural Net created:" + nn + "\nResult Matrix:");
 
+        //create result array
         double[][][] formatted_results = CSVReader.formatResultMatrix(structure, results);
-
         for(int i = 0; i < formatted_results.length; i++) {
             for(int j = 0; j < formatted_results[i].length; j++) {
                 if(j == 0) {
-                    System.out.println("Input " + (i + 1) + ":\n" + Arrays.toString(formatted_results[i][j]));
+                    System.out.println("Input " + (i) + ":\n" + Arrays.toString(formatted_results[i][j]));
                     continue;
                 }
                 else if(j == 1) {
-                    System.out.println("Output " + (i + 1) + ":\n" + Arrays.toString(formatted_results[i][j]) + "\n");
+                    System.out.println("Output " + (i) + ":\n" + Arrays.toString(formatted_results[i][j]) + "\n");
                 }
             }
         }
 
-        nn.train(formatted_results[0][0], formatted_results[0][1]);
+        System.out.print("Choose dataset used for training (number behind Input/Output in Result Matrix): ");
+        int index = reader.nextInt();
 
-        System.out.println(nn);
+        //train the net with one data sample of the result array
+        nn.train(formatted_results[index][0], formatted_results[index][1]);
 
-        double[][] nn_results = new double[formatted_results.length][];
-        for(int i = 0; i < nn_results.length; i++) {
-            nn_results[i] = nn.compute(formatted_results[i][0]);
-            System.out.println("Input: " + Arrays.toString(formatted_results[i][0]) + " Output nn: " + Arrays.toString(nn_results[i]) + " Expected: " + Arrays.toString(formatted_results[i][1]));
-        }
+        //trained network
+        System.out.println("Trained Network:" + nn + "\n");
 
-        //CSVReader.write("Tests/csv/testresults.csv", nn_results);
+        //make a prediction
+        double[] prediction = nn.compute(formatted_results[index][0]);
+        System.out.println("Prediction:\n" + "Input: " + Arrays.toString(formatted_results[index][0]) + " Output nn: " + Arrays.toString(prediction) + " Expected: " + Arrays.toString(formatted_results[index][1]));
     }
 }
